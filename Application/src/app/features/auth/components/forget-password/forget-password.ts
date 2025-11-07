b653a68341238e2ac206fad7ad35b84ae727e7f5
sondosEssam <sondosessam713@gmail.com>
2025-11-08 00:57:17 +0200

    forget passwrod

--- Application/src/app/features/auth/components/forget-password/forget-password.ts
+++ Application/src/app/features/auth/components/forget-password/forget-password.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormInput } from "../../../../shared/UI/form-input/form-input";
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AuthButton } from "../../layout/auth-button/auth-button";
+import { AuthChoice } from '../../services/auth-choice';
 
 @Component({
   selector: 'app-forget-password',
-  imports: [FormInput],
+  imports: [FormInput, ReactiveFormsModule, AuthButton],
   templateUrl: './forget-password.html',
   styleUrl: './forget-password.css',
 })
 export class ForgetPassword {
-
+fb = inject(FormBuilder);
+authChoiceService = inject(AuthChoice);
+formForgetPassword = this.fb.group({
+  email:['']
+});
+setAuthChoiceLogin(){
+  this.authChoiceService.setAuthChoice('register');
+}
+onSubmit(){
+  console.log(this.formForgetPassword.value);
+}
 }
