cfd163d4ecc17be9ca1e612ec5483612432014e7
sondosEssam <sondosessam713@gmail.com>
2025-11-10 01:16:03 +0200

    project restructure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Auth } from "./features/auth/auth";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Auth],
+  imports: [ Auth],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
