845a7acca657672e3ebc33097324926eadb7d428
sondosEssam <sondosessam713@gmail.com>
2025-11-24 18:13:07 +0200

    Authapis any type resolved to match service

--- projects/auth/src/lib/base/AuthApi.ts
+++ projects/auth/src/lib/base/AuthApi.ts
@@ -1,13 +1,15 @@
 import { Observable } from "rxjs";
-
+import * as authData from "../interfaces/auth-data";
+import { AuthApiAdaptor, AuthAdapted } from "../interfaces/adaptor";
 export abstract class AuthApi {
-    abstract login(data:any):Observable<any>;
-    abstract register(data:any):Observable<any>;
-    abstract changePassword(data:any):Observable<any>;
-    abstract forgotPassword(data:any):Observable<any>;
-    abstract verifyResetCode(data:any):Observable<any>;
-    abstract resetPassword(data:any):Observable<any>;
+    abstract login(data:authData.loginData):Observable<AuthApiAdaptor>;
+    abstract register(data:authData.registerData):Observable<AuthApiAdaptor>;
+    abstract forgotPassword(data:authData.forgetPasswordPasswordData):Observable<number>;
+    abstract verifyResetCode(data:authData.verifyResetCodeData):Observable<number>;
+    abstract resetPassword(data:authData.resetPasswordData):Observable<AuthApiAdaptor>;
+    //later on 
     abstract deleteMe(data:any):Observable<any>;
     abstract editProfile(data:any):Observable<any>;
+    abstract changePassword(data:any):Observable<any>;
     abstract logout(data:any):Observable<any>;
 }
