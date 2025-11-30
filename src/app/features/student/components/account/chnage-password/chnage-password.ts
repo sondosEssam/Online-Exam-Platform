cfbb921f9489a90c65279e403245f9ac64717789
sondosEssam <sondosessam713@gmail.com>
2025-11-30 22:50:26 +0200

    adding account sub components

---
+++ src/app/features/student/components/account/chnage-password/chnage-password.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chnage-password',
+  imports: [],
+  templateUrl: './chnage-password.html',
+  styleUrl: './chnage-password.css',
+})
+export class ChnagePassword {
+
+}
