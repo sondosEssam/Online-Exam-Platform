0583f97a3ede47e8cd198bcc661acd2c3cee09e9
sondosEssam <sondosessam713@gmail.com>
2025-12-02 13:10:37 +0200

    breedcrumb basic done

--- src/app/features/student/shared/breedcamb/breedcamb.ts
+++ src/app/features/student/shared/breedcamb/breedcamb.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-breedcamb',
-  imports: [BreadcrumbModule],
   templateUrl: './breedcamb.html',
-  styleUrl: './breedcamb.css',
+  imports: [BreadcrumbModule],
+  styleUrl: './breedcamb.css'
 })
-export class Breedcamb {
+export class Breedcamb implements OnInit {
 
+  items: MenuItem[] = [];
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    // Build breadcrumb on every navigation
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.buildBreadcrumb();
+    });
+
+    // Initial build
+    this.buildBreadcrumb();
+  }
+
+  private buildBreadcrumb() {
+    const breadcrumbs: MenuItem[] = [];
+
+    breadcrumbs.push({ label: 'Home', routerLink: '/student/diploma' });
+    const chidlren = this.route.children;
+    for (let child of chidlren) {
+      if (child && child.snapshot.data['Breadcrumb']!='Diploma') {
+        const snapshot = child.snapshot;
+        const label = snapshot.data['Breadcrumb'];
+        const url = snapshot.url.map(s => s.path).join('/');
+        if (label) {
+          breadcrumbs.push({ label, routerLink: `/student/${url}` });
+        }
+      }
+    }
+    this.items = breadcrumbs;
+  }
 }
--- src/app/features/student/student.routes.ts
+++ src/app/features/student/student.routes.ts
@@ -1,8 +1,9 @@
+
 export const studentRoutes=[
-    {path:'student', loadComponent:()=>import('./student').then(m=>m.Student), 
+    {path:'student', loadComponent:()=>import('./student').then(m=>m.Student),data:{Breadcrumb:'Home'}, 
         children:[
-            {path:'diploma', loadComponent:()=>import('./components/diploma/diploma').then(m=>m.Diploma)},
-            {path:'account', loadComponent:()=>import('./components/account/account').then(m=>m.Account)},
+            {path:'diploma', loadComponent:()=>import('./components/diploma/diploma').then(m=>m.Diploma), data:{Breadcrumb:'Diploma'}},
+            {path:'account', loadComponent:()=>import('./components/account/account').then(m=>m.Account), data:{Breadcrumb:'Account'}},
             // {path:'student', loadComponent:()=>import('./student').then(m=>m.Student)},
             // {path:'student', loadComponent:()=>import('./student').then(m=>m.Student)},
         ]
