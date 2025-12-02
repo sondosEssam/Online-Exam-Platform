6bd15bf423a33c289580dfe35ff71212458a5830
sondosEssam <sondosessam713@gmail.com>
2025-12-02 13:47:50 +0200

    adding shared headings

--- src/app/features/student/components/account/account.ts
+++ src/app/features/student/components/account/account.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { BackButton } from "../../shared/back-button/back-button";
+import { Headtag } from '../../shared/headtag/headtag';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [BackButton, Headtag],
   templateUrl: './account.html',
   styleUrl: './account.css',
 })
