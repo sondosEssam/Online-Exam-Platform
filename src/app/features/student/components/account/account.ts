9fed7e8b321d1ffcb72082573d17d39e38740f8d
sondosEssam <sondosessam713@gmail.com>
2025-12-02 14:24:32 +0200

    basic account setting layout  ui done

--- src/app/features/student/components/account/account.ts
+++ src/app/features/student/components/account/account.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BackButton } from "../../shared/back-button/back-button";
 import { Headtag } from '../../shared/headtag/headtag';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-account',
-  imports: [BackButton, Headtag],
+  imports: [BackButton, Headtag, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './account.html',
   styleUrl: './account.css',
 })
--- src/app/features/student/student.routes.ts
+++ src/app/features/student/student.routes.ts
@@ -1,11 +1,17 @@
+import { Routes } from '@angular/router';
 
-export const studentRoutes=[
+export const studentRoutes: Routes=[
     {path:'student', loadComponent:()=>import('./student').then(m=>m.Student),data:{Breadcrumb:'Home'}, 
         children:[
+            {path:'', redirectTo: 'diploma', pathMatch: 'full'},
             {path:'diploma', loadComponent:()=>import('./components/diploma/diploma').then(m=>m.Diploma), data:{Breadcrumb:'Diploma'}},
-            {path:'account', loadComponent:()=>import('./components/account/account').then(m=>m.Account), data:{Breadcrumb:'Account'}},
-            // {path:'student', loadComponent:()=>import('./student').then(m=>m.Student)},
-            // {path:'student', loadComponent:()=>import('./student').then(m=>m.Student)},
+            {path:'account', loadComponent:()=>import('./components/account/account').then(m=>m.Account), data:{Breadcrumb:'Account'},
+            children:[
+                    { path: '', redirectTo: 'profile', pathMatch: 'full' },
+                    {path:'profile', loadComponent:()=>import('./components/account/profile/profile').then(m=>m.Profile), data:{Breadcrumb:'Profile'}},
+                    {path:'changePassword', loadComponent:()=>import('./components/account/chnage-password/chnage-password').then(m=>m.ChnagePassword), data:{Breadcrumb:'Change Password'}}
+            ]
+        },
         ]
     }
 ]
