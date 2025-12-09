f54399453ffe9fabcba1623284bb4d96b91f78e2
sondosEssam <sondosessam713@gmail.com>
2025-12-09 02:24:05 +0200

    feature/ logout

--- src/app/features/student/components/account/account.ts
+++ src/app/features/student/components/account/account.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { routes } from './../../../../app.routes';
+import { Token } from './../../../../core/services/token';
+import { ModalService } from './../../../../shared/services/modal-service';
+import { Component, inject } from '@angular/core';
 import { BackButton } from "../../shared/back-button/back-button";
 import { Headtag } from '../../shared/headtag/headtag';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ColorDir } from '../../../../core/directives/color-dir';
+import { AuthLibraryService } from 'auth';
 
 @Component({
   selector: 'app-account',
@@ -11,5 +15,17 @@ import { ColorDir } from '../../../../core/directives/color-dir';
   styleUrl: './account.css',
 })
 export class Account {
+ModalService = inject(ModalService);
+authSerivce = inject(AuthLibraryService);
+Token = inject(Token);
+router = inject(Router);
+  logOut(){
+    this.authSerivce.logout().subscribe((res) => {
+      this.router.navigate(['/student/diploma']).then(()=>{
+        this.ModalService.triggerModal('Logged out successfully', 'success');
+        this.Token.clearToken();
+      });
+    });
+  }
 
 }
