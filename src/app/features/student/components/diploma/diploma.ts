afc59e3b94877451dba982d7c0c7fa22c1b377bb
sondosEssam <sondosessam713@gmail.com>
2025-12-11 15:56:59 +0200

    adding diploma ui

--- src/app/features/student/components/diploma/diploma.ts
+++ src/app/features/student/components/diploma/diploma.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Headtag } from "../../shared/headtag/headtag";
+import { Subjects } from '../../services/subjects';
+import { SubjectCard } from "../../shared/subject-card/subject-card";
 
 @Component({
   selector: 'app-diploma',
-  imports: [Headtag],
+  imports: [Headtag, SubjectCard],
   templateUrl: './diploma.html',
   styleUrl: './diploma.css',
 })
 export class Diploma {
-
+_subjectService = inject(Subjects);
+subjects: any[] = [];
+  getSubjects() {
+  this._subjectService.getSubjects().subscribe(
+    (res:any) =>
+       {
+    this.subjects = res;
+    console.log(this.subjects);
+      });
+}
+ngOnInit(){
+  this.getSubjects();
+}
 }
--- src/app/features/student/services/subjects.ts
+++ src/app/features/student/services/subjects.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { BaseUrl } from 'auth';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Subjects {
+  private _httpClient = inject(HttpClient);
+  private baseUrl = inject(BaseUrl);
   getSubjects() {
-    
+    return this._httpClient.get(`${this.baseUrl}/subjects`).pipe(map((res:any)=>res.subjects));
   }
 }
